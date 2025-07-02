51a0302d401a6fe09b08b6f679acccb6039553a2
seetharam j <seetharamj704@gmail.com>
2025-07-02 09:56:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,52 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ec2_instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+}
+
+variable "db_name" {
+  description = "RDS database name"
+  type        = string
+  default     = "mydb"
+}
+
+variable "db_username" {
+  description = "RDS database username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "RDS database password"
+  type        = string
+  sensitive   = true
+}
