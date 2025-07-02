7aa01003f353d1a2101c51b639a36d08b329d806
seetharam j <seetharamj704@gmail.com>
2025-07-02 10:12:11 +0530

    Create outputs.tf

---
+++ modules/ec2/outputs.tf
@@ -0,0 +1,14 @@
+output "web_sg_id" {
+  description = "Security group ID of web instances"
+  value       = aws_security_group.web_sg.id
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.web.dns_name
+}
+
+output "asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.web.name
+}
