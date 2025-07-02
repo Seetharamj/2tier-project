1b001ed6f13dd7cc3f77fd6af52ed73310823573
seetharam j <seetharamj704@gmail.com>
2025-07-02 13:40:25 +0530

    Update outputs.tf

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,20 +1,9 @@
-output "rds_endpoint" {
-  description = "RDS instance endpoint"
-  value       = aws_db_instance.default.endpoint
+output "db_endpoint" {
+  description = "Database endpoint"
+  value       = aws_db_instance.db.endpoint
 }
 
-output "rds_sg_id" {
-  description = "Security group ID of RDS instance"
-  value       = aws_security_group.rds_sg.id
-}
-
-output "db_name" {
-  description = "Database name"
-  value       = aws_db_instance.default.db_name
-}
-
-output "db_username" {
-  description = "Database username"
-  value       = aws_db_instance.default.username
-  sensitive   = true
+output "db_arn" {
+  description = "Database ARN"
+  value       = aws_db_instance.db.arn
 }
