0ed0b54692e785e99601cc824a71cfa5f599864d
seetharam j <seetharamj704@gmail.com>
2025-07-02 10:04:20 +0530

    Create main.tf

---
+++ modules/rds/main.tf
@@ -0,0 +1,87 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "env_prefix" {
+  description = "Environment prefix for resource names"
+  type        = string
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.env_prefix}-rds-sg"
+  description = "Security group for RDS"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [var.web_sg_id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rds-sg"
+  }
+}
+
+resource "aws_db_subnet_group" "rds" {
+  name       = "${var.env_prefix}-rds-subnet-group"
+  subnet_ids = var.private_subnets
+
+  tags = {
+    Name = "${var.env_prefix}-rds-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
+  parameter_group_name   = "default.mysql5.7"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  multi_az               = false
+
+  tags = {
+    Name = "${var.env_prefix}-rds-instance"
+  }
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.default.endpoint
+}
