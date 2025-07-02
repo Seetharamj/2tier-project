0633112a0a9a1ba2064a8d40ad8315e6a96e2ed5
seetharam j <seetharamj704@gmail.com>
2025-07-02 10:11:06 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,44 +1,10 @@
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "public_subnets" {
-  description = "List of public subnet IDs"
-  type        = list(string)
-}
-
-variable "private_subnets" {
-  description = "List of private subnet IDs"
-  type        = list(string)
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Name of the EC2 key pair"
-  type        = string
-}
-
-variable "env_prefix" {
-  description = "Environment prefix for resource names"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instances"
-  type        = string
-}
-
 resource "aws_security_group" "web_sg" {
   name        = "${var.env_prefix}-web-sg"
   description = "Security group for web servers"
   vpc_id      = var.vpc_id
 
   ingress {
+    description = "HTTP from anywhere"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -46,6 +12,7 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
+    description = "SSH from anywhere"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -60,31 +27,8 @@ resource "aws_security_group" "web_sg" {
   }
 
   tags = {
-    Name = "${var.env_prefix}-web-sg"
-  }
-}
-
-resource "aws_security_group" "alb_sg" {
-  name        = "${var.env_prefix}-alb-sg"
-  description = "Security group for ALB"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-alb-sg"
+    Name        = "${var.env_prefix}-web-sg"
+    Environment = var.env_prefix
   }
 }
 
@@ -93,6 +37,12 @@ resource "aws_launch_template" "web" {
   image_id      = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  user_data     = base64encode(templatefile("${path.module}/user_data.sh", {
+    db_endpoint = var.db_endpoint
+    db_name     = var.db_name
+    db_username = var.db_username
+    db_password = var.db_password
+  }))
 
   network_interfaces {
     associate_public_ip_address = true
@@ -102,32 +52,25 @@ resource "aws_launch_template" "web" {
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "${var.env_prefix}-web-server"
+      Name        = "${var.env_prefix}-web-server"
+      Environment = var.env_prefix
     }
   }
-
-  user_data = base64encode(<<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              echo "<h1>Hello from $(hostname -f)</h1>" > /var/www/html/index.html
-              EOF
-             )
 }
 
 resource "aws_autoscaling_group" "web" {
   name_prefix         = "${var.env_prefix}-web-asg-"
   vpc_zone_identifier = var.public_subnets
-  desired_capacity    = 2
-  min_size            = 2
-  max_size            = 4
+  desired_capacity    = var.desired_capacity
+  min_size            = var.min_size
+  max_size            = var.max_size
 
   launch_template {
     id      = aws_launch_template.web.id
     version = "$Latest"
   }
+
+  target_group_arns = [aws_lb_target_group.web.arn]
 
   tag {
     key                 = "Name"
@@ -144,7 +87,8 @@ resource "aws_lb" "web" {
   subnets            = var.public_subnets
 
   tags = {
-    Name = "${var.env_prefix}-web-alb"
+    Name        = "${var.env_prefix}-web-alb"
+    Environment = var.env_prefix
   }
 }
 
@@ -164,11 +108,6 @@ resource "aws_lb_target_group" "web" {
   }
 }
 
-resource "aws_autoscaling_attachment" "web" {
-  autoscaling_group_name = aws_autoscaling_group.web.id
-  lb_target_group_arn    = aws_lb_target_group.web.arn
-}
-
 resource "aws_lb_listener" "web" {
   load_balancer_arn = aws_lb.web.arn
   port              = "80"
@@ -180,10 +119,28 @@ resource "aws_lb_listener" "web" {
   }
 }
 
-output "web_instance_public_ips" {
-  value = aws_autoscaling_group.web.*.instances[*].public_ip
-}
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.env_prefix}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = var.vpc_id
 
-output "alb_dns_name" {
-  value = aws_lb.web.dns_name
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.env_prefix}-alb-sg"
+    Environment = var.env_prefix
+  }
 }
