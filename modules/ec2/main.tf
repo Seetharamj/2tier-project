f521ab4cd48313738b41d1be6b002159ad07c9c0
seetharam j <seetharamj704@gmail.com>
2025-07-02 10:03:17 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,189 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+}
+
+variable "env_prefix" {
+  description = "Environment prefix for resource names"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "${var.env_prefix}-web-sg"
+  description = "Security group for web servers"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-web-sg"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.env_prefix}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-alb-sg"
+  }
+}
+
+resource "aws_launch_template" "web" {
+  name_prefix   = "${var.env_prefix}-web-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.web_sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.env_prefix}-web-server"
+    }
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from $(hostname -f)</h1>" > /var/www/html/index.html
+              EOF
+             )
+}
+
+resource "aws_autoscaling_group" "web" {
+  name_prefix         = "${var.env_prefix}-web-asg-"
+  vpc_zone_identifier = var.public_subnets
+  desired_capacity    = 2
+  min_size            = 2
+  max_size            = 4
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.env_prefix}-web-asg"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "web" {
+  name               = "${var.env_prefix}-web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.public_subnets
+
+  tags = {
+    Name = "${var.env_prefix}-web-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web" {
+  name     = "${var.env_prefix}-web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_autoscaling_attachment" "web" {
+  autoscaling_group_name = aws_autoscaling_group.web.id
+  lb_target_group_arn    = aws_lb_target_group.web.arn
+}
+
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
+
+output "web_instance_public_ips" {
+  value = aws_autoscaling_group.web.*.instances[*].public_ip
+}
+
+output "alb_dns_name" {
+  value = aws_lb.web.dns_name
+}
