f9c0613029846b9fead5faac12514c18e38edac1
seetharam j <seetharamj704@gmail.com>
2025-07-02 10:08:06 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "env_prefix" {
+  description = "Environment prefix for resource names and tags"
+  type        = string
+  default     = "dev"
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets to create"
+  type        = number
+  default     = 2
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets to create"
+  type        = number
+  default     = 2
+}
