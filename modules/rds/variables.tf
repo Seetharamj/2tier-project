e4a61dc6edcb5964b8744d496e25e894c5f8d9df
seetharam j <seetharamj704@gmail.com>
2025-07-02 13:40:07 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,28 +1,11 @@
-variable "vpc_id" {
-  description = "VPC ID where RDS will be created"
-  type        = string
-}
-
-variable "private_subnets" {
-  description = "List of private subnet IDs"
-  type        = list(string)
-}
-
-variable "web_sg_id" {
-  description = "Security group ID of web instances"
-  type        = string
-}
-
 variable "db_name" {
   description = "Database name"
   type        = string
-  default     = "mydb"
 }
 
 variable "db_username" {
   description = "Database username"
   type        = string
-  default     = "admin"
 }
 
 variable "db_password" {
@@ -31,44 +14,24 @@ variable "db_password" {
   sensitive   = true
 }
 
-variable "env_prefix" {
-  description = "Environment prefix for naming"
+variable "db_instance_class" {
+  description = "Database instance class"
   type        = string
-  default     = "dev"
+  default     = "db.t3.micro"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security group ID"
+  type        = string
 }
 
 variable "allocated_storage" {
   description = "Allocated storage in GB"
   type        = number
   default     = 20
-}
-
-variable "storage_type" {
-  description = "Storage type (standard, gp2, io1)"
-  type        = string
-  default     = "gp2"
-}
-
-variable "engine_version" {
-  description = "MySQL engine version"
-  type        = string
-  default     = "5.7"
-}
-
-variable "instance_class" {
-  description = "RDS instance class"
-  type        = string
-  default     = "db.t3.micro"
-}
-
-variable "multi_az" {
-  description = "Enable multi-AZ deployment"
-  type        = bool
-  default     = false
-}
-
-variable "backup_retention_period" {
-  description = "Backup retention period in days"
-  type        = number
-  default     = 7
 }
