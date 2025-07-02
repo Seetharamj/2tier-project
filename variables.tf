40f0f7a33e025aa0356a22e4253b33e58a9d52ba
seetharam j <seetharamj704@gmail.com>
2025-07-02 17:57:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+# Global Configuration
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
@@ -5,17 +6,18 @@ variable "aws_region" {
 }
 
 variable "project_name" {
-  description = "Name of the project"
+  description = "Name of the project (used for resource naming)"
   type        = string
   default     = "2tier-app"
 }
 
 variable "environment" {
-  description = "Environment name"
+  description = "Deployment environment (dev/stage/prod)"
   type        = string
   default     = "dev"
 }
 
+# Network Configuration
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
@@ -23,25 +25,26 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnets" {
-  description = "List of public subnets"
+  description = "List of public subnet CIDRs"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
-  description = "List of private subnets"
+  description = "List of private subnet CIDRs"
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "database_subnets" {
-  description = "List of database subnets"
+  description = "List of database subnet CIDRs"
   type        = list(string)
   default     = ["10.0.5.0/24", "10.0.6.0/24"]
 }
 
+# Web Tier Configuration
 variable "web_instance_count" {
-  description = "Number of web instances"
+  description = "Number of web tier instances"
   type        = number
   default     = 2
 }
@@ -55,58 +58,81 @@ variable "web_instance_type" {
 variable "web_ami" {
   description = "AMI ID for web servers"
   type        = string
-  default     = "ami-0fc5d935ebf8bc3bc"
+  default     = "ami-0fc5d935ebf8bc3bc" # Amazon Linux 2023
 }
 
+# Application Tier Configuration
 variable "app_instance_count" {
-  description = "Number of app instances"
+  description = "Number of application tier instances"
   type        = number
   default     = 2
 }
 
 variable "app_instance_type" {
-  description = "Instance type for app servers"
+  description = "Instance type for application servers"
   type        = string
   default     = "t3.micro"
 }
 
 variable "app_ami" {
-  description = "AMI ID for app servers"
+  description = "AMI ID for application servers"
   type        = string
-  default     = "ami-0fc5d935ebf8bc3bc"
+  default     = "ami-0fc5d935ebf8bc3bc" # Amazon Linux 2023
 }
 
-variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-}
-
+# Database Configuration
 variable "db_name" {
-  description = "Database name"
+  description = "RDS database name"
   type        = string
   default     = "mydb"
 }
 
 variable "db_username" {
-  description = "Database username"
+  description = "RDS master username"
   type        = string
   default     = "admin"
 }
 
 variable "db_password" {
-  description = "Database password"
+  description = "RDS master password"
   type        = string
   sensitive   = true
 }
 
 variable "db_instance_class" {
-  description = "Database instance class"
+  description = "RDS instance type"
   type        = string
   default     = "db.t3.micro"
 }
 
 variable "allocated_storage" {
-  description = "Allocated storage for database in GB"
+  description = "RDS storage size in GB"
   type        = number
   default     = 20
+}
+
+# Security Configuration
+variable "key_name" {
+  description = "Name of existing EC2 key pair for SSH access"
+  type        = string
+}
+
+# Load Balancer Configuration
+variable "lb_target_group_arn" {
+  description = "ARN of the load balancer target group for web tier"
+  type        = string
+  default     = ""
+}
+
+variable "create_lb_attachment" {
+  description = "Whether to create load balancer attachments for web tier"
+  type        = bool
+  default     = true
+}
+
+# Tags Configuration
+variable "additional_tags" {
+  description = "Additional tags to apply to all resources"
+  type        = map(string)
+  default     = {}
 }
